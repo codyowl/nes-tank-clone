19aec9d67ea259887d835e85ba22f4b41f03b099
codyowl <codyowl@gmail.com>
2021-07-11 03:57:28 +0530

    added player movement still needs some debugging

--- game.py
+++ game.py
@@ -7,27 +7,59 @@ game_world_y = 400
 frames_per_second = 40
 animation_cycle = 4
 
-class NESTank:
-	def __init__(self):
-		self.clock = pygame.time.Clock()
 
+class NESTank:
+	
 	# initial level setup and pygame initiation
 	def game_setup(self):
-		pygame.init()
 		self.game_world = pygame.display.set_mode([game_world_x,game_world_y])
 		self.game_background = pygame.image.load(os.path.join('sprites','main_background_with_bricks.png'))
+		self.clock = pygame.time.Clock()
+		pygame.init()
 		self.game_background = pygame.transform.scale(self.game_background, (800, 400))
 		self.game_background_size = self.game_world.get_rect()
 	
 	def game_main_loop(self):
 		while True:
+			# importing from players class
 			hero_tank = HeroTank()
 			hero_tank.rect.x = 0
 			hero_tank.rect.y = 0
 			players_list = pygame.sprite.Group()
 			players_list.add(hero_tank)
+			steps = 10
+
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					try:
+						sys.exit()
+					finally:
+						break
+
+				# for long press keydown
+				if event.type == pygame.KEYDOWN:
+					if event.key == ord('q'):
+						pygame.quit()
+						try:
+							sys.exit()
+						finally:
+							break
+
+					if event.key == pygame.K_LEFT or event.key == ord('a'):
+						hero_tank.control(-steps, 0)
+					if event.key == pygame.K_RIGHT or event.key == ord('d'):
+						hero_tank.control(steps, 0)
+
+				# for single press
+				if event.type == pygame.KEYUP:
+					if event.key == pygame.K_LEFT or event.key == ord('a'):
+						hero_tank.control(steps, 0)
+					if event.key == pygame.K_RIGHT or event.key == ord('d'):
+						hero_tank.control(-steps, 0)    
 
 			self.game_world.blit(self.game_background, self.game_background_size)
+			hero_tank.update()
 			players_list.draw(self.game_world)
 			pygame.display.flip()
 			self.clock.tick(frames_per_second)
--- players/hero_tank.py
+++ players/hero_tank.py
@@ -1,13 +1,38 @@
 import pygame
 import os
 
+animation_cycle = 4
+
 class HeroTank(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
+		self.tank_movement_x = 0
+		self.tank_movement_y = 0
+		self.frame = 0
 		self.images = []
-
 		hero_tank_image = pygame.image.load(os.path.join('sprites', 'hero_tank.png')).convert()
 		self.images.append(hero_tank_image)
 		self.image = self.images[0]
 		self.rect = self.image.get_rect()
 
+	def control(self, x_direction, y_direction):
+		self.tank_movement_x += x_direction
+		self.tank_movement_y += y_direction
+
+	def update(self):
+		self.rect.x = self.rect.x + self.tank_movement_x    
+		self.rect.y = self.rect.y + self.tank_movement_y
+
+		# moving left
+		if self.tank_movement_x  < 0:
+			self.frame += 1
+			if self.frame > 3*animation_cycle:
+				self.frame = 0
+			self.image = pygame.transform.flip(self.images[self.frame//animation_cycle], True, False)
+
+		# moving right
+		if self.tank_movement_x > 0:
+			self.frame += 1
+			if self.frame > 3*animation_cycle:
+				self.frame = 0
+			self.image = self.images[self.frame//animation_cycle]
